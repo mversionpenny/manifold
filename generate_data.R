41fd99b32b67b94c195c60d453317b816ddb72f4
Hoai Thu Nguyen <hoaithu.nguyen.26.4.92@gmail.com>
2016-11-29 20:06:49 +0100

    Correct generete_data.R - shorter code - Color as in articles - Broken Swiss Roll with 5000 obs - plot with titles - change angles of plots of twinpeaks - test in comment after each function - newHD

--- generate_data.R
+++ generate_data.R
@@ -1,176 +1,101 @@
-#------------------- Margot Selosse -------------------
-#----------------- Manifold Learning ------------------
-#------------------ 15 oct. 2016 ----------------------
+#--------- Margot Selosse, Hoai Thu Nguyen, Maram Romdhane -------------
+#------------------------ Manifold Learning Prject----------------------
+#------------------------------ 2016/2017 ------------------------------
 
-#install.packages("plot3D")
-#install.packages("rgl")
 library(rgl)
 library(plot3D)
-observationsNum <- 10000
+observationsNum <- 5000
 noise <- 0.05
 
-
 #### Swiss ROll ####
-#TODO : title to the plot + comments
-
 swissroll <- function(n,noise){
-  p <- runif(n)
+  p <- sort(runif(n))
   q <- runif(n)
   t <- 3*pi/2 * (1 + 2*p)
   X <- cbind(t*cos(t), t*sin(t), 30*q) + noise * rnorm(n)
 
-  x <- t(X[,1])
-  y <- t(X[,2])
-  z <- t(X[,3])
-  par(mfrow = c(1, 2))
-  scatter3D(x,y,z, phi= 80, theta=90) 
-  scatter3D(x,y,z, phi= 40, theta=90) 
-  plot3d(x,y,z, type="p",aspect =TRUE,col=rainbow(5))
-  
+  par(mfrow = c(1, 2), oma = c(0, 0, 2, 0))
+  scatter3D(X[,1],X[,2],X[,3], phi= -60, theta=90, colvar = t, colkey=F, pch=20, cex=0.75)
+  scatter3D(X[,1],X[,2],X[,3], phi= 40, theta=90, colvar = t, colkey=F, pch=20, cex=0.75)  
+  mtext("Simulation of a Swiss Roll dataset", outer = TRUE, cex = 1.5)
+  plot3d(X[,1],X[,2],X[,3], type="p",aspect =TRUE, col=jet.col(n), main = "Simulation of a Swiss Roll dataset")
   return(X)
 }
+#swiss <- swissroll(observationsNum,noise)
 
 
 #### Broken Swiss Roll ####
-#/!\didn't understand interval of rejected ti!
-
-brokenSwissroll <- function(n, noise){
-  p <- runif(n)
+brokenSwissroll <- function(n, noise, n.break){
+  p <- sort(runif(n+n.break))
   q <- runif(n)
   t <- 3*pi/2 * (1 + 2*p)
-  keptIndex1 <- which(t<8,arr.ind = TRUE)
-  keptIndex2 <- which(t>10,arr.ind = TRUE)
-  generalKeptIndex <- sort(c(keptIndex1,keptIndex2))
-  t <- t[generalKeptIndex]
-  X <- cbind(t*cos(t), t*sin(t), 30*q[generalKeptIndex]) + noise * rnorm(n)[generalKeptIndex]
+  colors <- jet.col(n+n.break)
   
-  x <- t(X[,1])
-  y <- t(X[,2])
-  z <- t(X[,3])
-  par(mfrow = c(1, 2))
-  scatter3D(x,y,z, phi= 80, theta=90) 
-  scatter3D(x,y,z, phi= 40, theta=90) 
-  plot3d(x,y,z, type="p",aspect =TRUE,col=rainbow(5))
+  begin.break <- which(t>8)[1]
+  t <- t[-c(begin.break:(begin.break+n.break-1))]
+  colors <- colors[-c(begin.break:(begin.break+n.break-1))]
+  X <- cbind(t*cos(t), t*sin(t), 30*q) + noise * rnorm(n)
   
+  par(mfrow = c(1, 2), oma = c(0, 0, 2, 0))
+  scatter3D(X[,1],X[,2],X[,3], phi= -60, theta=90, colvar = t, colkey=F, pch=20, cex=0.75, col=colors)
+  scatter3D(X[,1],X[,2],X[,3], phi= 40, theta=90, colvar = t, colkey=F, pch=20, cex=0.75, col=colors) 
+  mtext("Simulation of a broken Swiss Roll dataset", outer = TRUE, cex = 1.5)
+  plot3d(X[,1],X[,2],X[,3], type="p",aspect =TRUE, col=colors , main = "Simulation of a broken Swiss Roll dataset")
   return(X)
 }
+#broken.swiss <- brokenSwissroll(observationsNum,noise, 500)
 
 #### Helix ####
-# TODO: find how to do a good-looking helix 
-
+#colors is correcly broken too
 helix <- function(n, noise){
-  p <- runif(n)
-  X <- cbind((2+cos(8*p))*cos(p), (2+cos(8*p))*sin(p), sin(8*p)) + noise * rnorm(n)
-  
-  x <- t(X[,1])
-  y <- t(X[,2])
-  z <- t(X[,3])
-  par(mfrow = c(1, 2))
-  scatter3D(x,y,z, phi= 80, theta=90) 
-  scatter3D(x,y,z, phi= 40, theta=90) 
-  plot3d(x,y,z, type="p",aspect =TRUE,col=rainbow(5))
-  
-  return(X)
-}
-
-newHelix <- function(n, noise){
   t <- (1:n)/n
   t <- t * 2 * pi
   X <- cbind((2+cos(8*t))*cos(t), (2+cos(8*t))*sin(t), sin(8*t)) + noise * rnorm(n)
   
-  x <- t(X[,1])
-  y <- t(X[,2])
-  z <- t(X[,3])
-  par(mfrow = c(1, 2))
-  scatter3D(x,y,z, phi= 80, theta=90) 
-  scatter3D(x,y,z, phi= 40, theta=90) 
-  plot3d(x,y,z, type="p",aspect =TRUE,col=rainbow(5))
-  
+  par(mfrow = c(1, 2), oma = c(0, 0, 2, 0))
+  scatter3D(X[,1],X[,2],X[,3], phi= -60, theta=90, colvar = t, colkey=F, pch=20, cex=0.75)
+  scatter3D(X[,1],X[,2],X[,3], phi= 40, theta=90, colvar = t, colkey=F, pch=20, cex=0.75)  
+  mtext("Simulation of a helix dataset", outer = TRUE, cex = 1.5)
+  plot3d(X[,1],X[,2],X[,3], type="p",aspect =TRUE, col=jet.col(n), main = "Simulation of a helix dataset")
   return(X)
 }
+#helix.data <- helix(observationsNum,noise)
 
 #### Twinpeaks ####
-
-wrongTwinpeaks <- function(n, noise){
-  p <- runif(n)
-  q <- runif(n)
-  X <- cbind(1-2*p, sin(pi - 2*pi*p), tanh(3 - 6*q)) + noise * rnorm(n)
-  
-  x <- t(X[,1])
-  y <- t(X[,2])
-  z <- t(X[,3])
-  par(mfrow = c(1, 2))
-  scatter3D(x,y,z, phi= 80, theta=90) 
-  scatter3D(x,y,z, phi= 40, theta=90) 
-  plot3d(x,y,z, type="p",aspect =TRUE,col=rainbow(5))
-
-  return(X)
-}
-
 twinpeaks <- function(n, noise){
   p <- runif(n)
   q <- runif(n)
   X <- cbind(1-2*p, 1-2*q, sin(pi - 2*pi*p) * tanh(3 - 6*q)) + noise * rnorm(n)
-  x <- t(X[,1])
-  y <- t(X[,2])
-  z <- t(X[,3]*10)
-  par(mfrow = c(1, 2))
-  scatter3D(x,y,z, phi= 80, theta=90) 
-  scatter3D(x,y,z, phi= 40, theta=90) 
-  plot3d(x,y,z, type="p",aspect =TRUE,col=rainbow(5))
+  X<- X[order(X[,3]),]
   
+  par(mfrow = c(1, 2), oma = c(0, 0, 2, 0))
+  scatter3D(X[,1],X[,2],X[,3], phi= 30, theta=-80, colvar = X[,3], colkey=F, pch=20, cex=0.65)
+  scatter3D(X[,1],X[,2],X[,3], phi= -30, theta=45, colvar = X[,3], colkey=F, pch=20, cex=0.65) 
+  mtext("Simulation of a twinpeaks dataset", outer = TRUE, cex = 1.5)
+  plot3d(X[,1],X[,2],X[,3], type="p",aspect =TRUE, col=jet.col(n), main = "Simulation of a twinpeaks dataset")
   return(X)
 }
+#twins <- twinpeaks(observationsNum,noise)
 
 #### HD ####
 
 HD <-function(n){
   ndims = 5
-  nb_per_dim = round(n ^(1/ndims))
+  nb_per_dim = round(n^(1/ndims))
   l <- list(seq(0,1,length=nb_per_dim))
   temp <- rep(t(l),ndims)
   t <- expand.grid(temp)
   X <- cbind(cos(t[,1]), tanh(3 * t[,2]), t[,1] + t[,3], t[,4] * sin(t[,2]), sin(t[,1] + t[,5]), t[,5] * cos(t[,2]), t[,5] + t[,4], t[,2], t[,3] * t[,4], t[,1])
-  
-  par(mfrow = c(3,3))
-  plot(X[,1], X[,3])
-  plot(X[,10], X[,7])
-  plot(X[,2], X[,5])
-  plot(X[,5], X[,9])
-  plot(X[,9], X[,8])
-  plot(X[,1], X[,4])
-  plot(X[,2], X[,10])
-  plot(X[,3], X[,9])
-  plot(X[,4], X[,10])
-  
-  scatter3D(X[,1],X[,2],X[,3]) 
-  plot3d(X[,1],X[,2],X[,3], type="p",aspect =TRUE,col=rainbow(5))
-  
-  scatter3D(X[,3],X[,4],X[,5]) 
-  plot3d(X[,3],X[,4],X[,5], type="p",aspect =TRUE,col=rainbow(5))
-  
-  scatter3D(X[,5],X[,6],X[,7]) 
-  plot3d(X[,5],X[,6],X[,7], type="p",aspect =TRUE,col=rainbow(5))
-  
-  scatter3D(X[,7],X[,8],X[,9]) 
-  plot3d(X[,7],X[,8],X[,9], type="p",aspect =TRUE,col=rainbow(5))
-  
-  scatter3D(X[,10],X[,1],X[,2]) 
-  plot3d(X[,7],X[,8],X[,9], type="p",aspect =TRUE,col=rainbow(5))
-  
-  scatter3D(X[,4],X[,6],X[,8]) 
-  plot3d(X[,7],X[,8],X[,9], type="p",aspect =TRUE,col=rainbow(5))
-  
-  scatter3D(X[,2],X[,4],X[,7]) 
-  plot3d(X[,7],X[,8],X[,9], type="p",aspect =TRUE,col=rainbow(5))
-  
-  scatter3D(X[,3],X[,7],X[,10]) 
-  plot3d(X[,7],X[,8],X[,9], type="p",aspect =TRUE,col=rainbow(5))
-  
-  scatter3D(X[,7],X[,8],X[,9]) 
-  plot3d(X[,7],X[,8],X[,9], type="p",aspect =TRUE,col=rainbow(5))
-  
   return(X)
 }
 
+newHD <-function(n){
+  ndims = 5
+  t <- matrix(runif(n*ndims), ncol=ndims)
+  # ou trouver les fonctions de combinaison? 
+  X <- cbind(cos(t[,1]), tanh(3 * t[,2]), t[,1] + t[,3], t[,4] * sin(t[,2]), sin(t[,1] + t[,5]), t[,5] * cos(t[,2]), t[,5] + t[,4], t[,2], t[,3] * t[,4], t[,1])
+  return(X)
+}
+
+hd <- HD(observationsNum)
 
