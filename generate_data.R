fe62dc663f906bc36ab456a6b0197e07905a888c
Hoai Thu Nguyen <hoaithu.nguyen.26.4.92@gmail.com>
2016-12-01 15:09:23 +0100

    generate_data.R: one 2D plot with best angle, optional 3D plot, openbox main_data.R: test  kpca

--- generate_data.R
+++ generate_data.R
@@ -1,31 +1,36 @@
 #--------- Margot Selosse, Hoai Thu Nguyen, Maram Romdhane -------------
 #------------------------ Manifold Learning Prject----------------------
 #------------------------------ 2016/2017 ------------------------------
-
+# Functions to generate different types of datasets
+list.of.packages <- c("rgl", "plot3D")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.rstudio.com/")
 library(rgl)
 library(plot3D)
-observationsNum <- 5000
-noise <- 0.05
 
 #### Swiss ROll ####
-swissroll <- function(n,noise){
+swissroll <- function(n = 5000, noise = 0.05, plot.3D = T){
   p <- sort(runif(n))
   q <- runif(n)
   t <- 3*pi/2 * (1 + 2*p)
   X <- cbind(t*cos(t), t*sin(t), 30*q) + noise * rnorm(n)
-
-  par(mfrow = c(1, 2), oma = c(0, 0, 2, 0))
-  scatter3D(X[,1],X[,2],X[,3], phi= -60, theta=90, colvar = t, colkey=F, pch=20, cex=0.75)
-  scatter3D(X[,1],X[,2],X[,3], phi= 40, theta=90, colvar = t, colkey=F, pch=20, cex=0.75)  
-  mtext("Simulation of a Swiss Roll dataset", outer = TRUE, cex = 1.5)
-  plot3d(X[,1],X[,2],X[,3], type="p",aspect =TRUE, col=jet.col(n), main = "Simulation of a Swiss Roll dataset")
+  
+  scatter3D(X[,3],X[,1],X[,2], phi= 15, theta=60, colvar = t, colkey=F, pch=16, 
+            bty = "b2", ticktype = "detailed", scale = F,
+            xlim = c(-5,35), ylim = c(-15,15), zlim = c(-10,15), xlab= "", ylab="", zlab="")
+  
+  if (plot.3D)
+    plot3d(X[,3],X[,1],X[,2], type="p",aspect =TRUE, col=jet.col(n),
+           xlab="", ylab="", zlab="", expand = 1.5, box=F, bty="b2", size = 7)
+  
   return(X)
 }
-#swiss <- swissroll(observationsNum,noise)
+#swiss <- swissroll()
 
 
 #### Broken Swiss Roll ####
-brokenSwissroll <- function(n, noise, n.break){
+#colors is correcly broken too
+brokenSwissroll <- function(n = 5000, noise = 0.05, n.break = 500, plot.3D = T){
   p <- sort(runif(n+n.break))
   q <- runif(n)
   t <- 3*pi/2 * (1 + 2*p)
@@ -36,50 +41,58 @@ brokenSwissroll <- function(n, noise, n.break){
   colors <- colors[-c(begin.break:(begin.break+n.break-1))]
   X <- cbind(t*cos(t), t*sin(t), 30*q) + noise * rnorm(n)
   
-  par(mfrow = c(1, 2), oma = c(0, 0, 2, 0))
-  scatter3D(X[,1],X[,2],X[,3], phi= -60, theta=90, colvar = t, colkey=F, pch=20, cex=0.75, col=colors)
-  scatter3D(X[,1],X[,2],X[,3], phi= 40, theta=90, colvar = t, colkey=F, pch=20, cex=0.75, col=colors) 
-  mtext("Simulation of a broken Swiss Roll dataset", outer = TRUE, cex = 1.5)
-  plot3d(X[,1],X[,2],X[,3], type="p",aspect =TRUE, col=colors , main = "Simulation of a broken Swiss Roll dataset")
+  scatter3D(X[,3],X[,1],X[,2], phi= 15, theta=60, colvar = t, colkey=F, pch=16, 
+            bty = "b2", ticktype = "detailed", scale = F,
+            xlim = c(-5,35), ylim = c(-15,15), zlim = c(-10,15), xlab= "", ylab="", zlab="")
+  
+  if (plot.3D)
+    plot3d(X[,3],X[,1],X[,2], type="p",aspect =TRUE, col=jet.col(n),
+           xlab="", ylab="", zlab="", expand = 1.5, box=F, bty="b2", size = 7)
+  
   return(X)
 }
-#broken.swiss <- brokenSwissroll(observationsNum,noise, 500)
+#broken.swiss <- brokenSwissroll()
 
 #### Helix ####
-#colors is correcly broken too
-helix <- function(n, noise){
+helix <- function(n = 5000, noise = 0.05, plot.3D=T){
   t <- (1:n)/n
   t <- t * 2 * pi
   X <- cbind((2+cos(8*t))*cos(t), (2+cos(8*t))*sin(t), sin(8*t)) + noise * rnorm(n)
   
-  par(mfrow = c(1, 2), oma = c(0, 0, 2, 0))
-  scatter3D(X[,1],X[,2],X[,3], phi= -60, theta=90, colvar = t, colkey=F, pch=20, cex=0.75)
-  scatter3D(X[,1],X[,2],X[,3], phi= 40, theta=90, colvar = t, colkey=F, pch=20, cex=0.75)  
-  mtext("Simulation of a helix dataset", outer = TRUE, cex = 1.5)
-  plot3d(X[,1],X[,2],X[,3], type="p",aspect =TRUE, col=jet.col(n), main = "Simulation of a helix dataset")
+  scatter3D(X[,1],X[,2],X[,3], phi= 15, theta=60, colvar = t, colkey=F, pch=20, 
+            bty = "b2", ticktype = "detailed", scale = F,
+            xlim = c(-3, 3), ylim = c(-3,3), zlim = c(-1.5,1.5), xlab= "", ylab="", zlab="")
+  
+  if (plot.3D)
+    plot3d(X[,1],X[,2],X[,3], type="p",aspect=c(2,1,1), col=jet.col(n),
+           xlab="", ylab="", zlab="", expand = 1.5, box=F, bty="b2", size = 7)
+
   return(X)
 }
-#helix.data <- helix(observationsNum,noise)
+#helix.data <- helix()
 
 #### Twinpeaks ####
-twinpeaks <- function(n, noise){
+twinpeaks <- function(n = 5000, noise= 0.02, plot.3D=T){
   p <- runif(n)
   q <- runif(n)
   X <- cbind(1-2*p, 1-2*q, sin(pi - 2*pi*p) * tanh(3 - 6*q)) + noise * rnorm(n)
   X<- X[order(X[,3]),]
   
-  par(mfrow = c(1, 2), oma = c(0, 0, 2, 0))
-  scatter3D(X[,1],X[,2],X[,3], phi= 30, theta=-80, colvar = X[,3], colkey=F, pch=20, cex=0.65)
-  scatter3D(X[,1],X[,2],X[,3], phi= -30, theta=45, colvar = X[,3], colkey=F, pch=20, cex=0.65) 
-  mtext("Simulation of a twinpeaks dataset", outer = TRUE, cex = 1.5)
-  plot3d(X[,1],X[,2],X[,3], type="p",aspect =TRUE, col=jet.col(n), main = "Simulation of a twinpeaks dataset")
+  scatter3D(X[,1],X[,2],X[,3], phi= 35, theta= -35, colvar = t, colkey=F, pch=16, 
+            bty = "b2", ticktype = "detailed", scale = F,
+            xlim = c(-1.2,1.2), ylim = c(-1.2,1.2), zlim = c(-1.2,1.2), xlab= "", ylab="", zlab="")
+  
+  if (plot.3D)
+    plot3d(X[,1],X[,2],X[,3], type="p",aspect=c(1,1,1), col=jet.col(n),
+           xlab="", ylab="", zlab="", expand = 1.5, box=F, bty="b2", size = 7)
+  
   return(X)
 }
-#twins <- twinpeaks(observationsNum,noise)
+#twins <- twinpeaks()
 
 #### HD ####
 
-HD <-function(n){
+HD <-function(n = 5000){
   ndims = 5
   nb_per_dim = round(n^(1/ndims))
   l <- list(seq(0,1,length=nb_per_dim))
@@ -89,13 +102,34 @@ HD <-function(n){
   return(X)
 }
 
-newHD <-function(n){
-  ndims = 5
-  t <- matrix(runif(n*ndims), ncol=ndims)
-  # ou trouver les fonctions de combinaison? 
-  X <- cbind(cos(t[,1]), tanh(3 * t[,2]), t[,1] + t[,3], t[,4] * sin(t[,2]), sin(t[,1] + t[,5]), t[,5] * cos(t[,2]), t[,5] + t[,4], t[,2], t[,3] * t[,4], t[,1])
+#hd <- HD()
+
+#### Open box ####
+
+openbox <- function(n = 5000, noise = 0.02, plot.3D = T){
+  nb_per_side = n %/% 6
+  nb_in_base = n - nb_per_side*5
+  #base
+  base <- replicate(2, runif(nb_in_base,-5,5))
+  base <- cbind(base,rep(0,nb_in_base))
+  #top
+  top <- cbind(runif(nb_per_side,5,15), runif(nb_per_side,-5,5), rep(10,nb_per_side))
+  #sides
+  s1 <- cbind(runif(nb_per_side,-5,5), rep(-5, nb_per_side), runif(nb_per_side, 0, 10))
+  s2 <- cbind(runif(nb_per_side,-5,5), rep(5, nb_per_side), runif(nb_per_side, 0, 10))
+  s3 <- cbind(rep(-5, nb_per_side), runif(nb_per_side,-5,5),  runif(nb_per_side, 0, 10))
+  s4 <-  cbind(rep(5, nb_per_side), runif(nb_per_side,-5,5),  runif(nb_per_side, 0, 10))
+  X <- rbind(base,s1,s2,s3,s4,top) + noise*rnorm(n)
+  X <- X[order(X[,3]), ]
+  
+  scatter3D(X[,1],X[,2],X[,3], phi= 35, theta= -35, colvar = t, colkey=F, pch=16, 
+            bty = "b2", ticktype = "detailed", scale = F,
+            xlim = c(-7, 17), ylim = c(-7,7), zlim = c(-2,12), xlab= "", ylab="", zlab="")
+  
+  if (plot.3D)
+    plot3d(X[,1],X[,2],X[,3], type="p",aspect=c(2,1,1), col=jet.col(n),
+           xlab="", ylab="", zlab="", expand = 1.5, box=F, bty="b2", pch = 16, size = 7)
+   
   return(X)
 }
-
-#hd <- HD(observationsNum)
-
+#openbox <- openbox()
--- main.R
+++ main.R
@@ -1,12 +1,25 @@
 #--------- Margot Selosse, Hoai Thu Nguyen, Maram Romdhane -------------
 #------------------------ Manifold Learning Prject----------------------
 #------------------------------ 2016/2017 ------------------------------
-list.of.packages <- c("rstudioapi")
+list.of.packages <- c("rstudioapi", "kernlab")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos = "http://cran.rstudio.com/")
 library(rstudioapi)
+library(kernlab)
+
+rm(list=ls())
+dev.off()
 
 # Load simulation data functions
 this.dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(this.dir)
 source("generate_data.R")
+
+roll <- swissroll(plot.3D = F) # no 3D plot
+realDist <- dist(roll)
+
+kpc <- kpca(~. , data = as.data.frame(roll), kernel="rbfdot", kpar=list(sigma=0.1), features=2)
+test <- rotated(kpc)
+plot(test)
+sum(realDist - dist(test))
+#with(text(test[,1], test[,2], labels = colnames(as.matrix(data))))
