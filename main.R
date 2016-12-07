7bb4a4fed55ee946b70add2631ed13307fc3cd52
Hoai Thu Nguyen <hoaithu.nguyen.26.4.92@gmail.com>
2016-12-07 18:02:16 +0100

    todo: run main.R tonight

--- main.R
+++ main.R
@@ -1,22 +1,49 @@
 #--------- Margot Selosse, Hoai Thu Nguyen, Maram Romdhane -------------
 #------------------------ Manifold Learning Prject----------------------
 #------------------------------ 2016/2017 ------------------------------
-list.of.packages <- c("rstudioapi")
+list.of.packages <- c("rstudioapi", "doParallel", "foreach")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos = "http://cran.rstudio.com/")
 library(rstudioapi)
+library(doParallel)
+library(foreach)
 
 rm(list=ls())
 dev.off()
 
-# Set working directory to the location of this file
+## Set working directory to the location of this file
 this.dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(this.dir)
 
-# Generate data ####
-# ATTENTION: Run only once
+#### Generate data ####
+## ATTENTION: Run only once
 # source("generate_data.R")
 
-# Optimize the parameters ####
+#### Read the artificial data ####
+swissRoll <- read.table("data/swissRoll.txt", sep = "\t")
+brokenSwissRoll <- read.table("data/brokenSwissRoll.txt", sep = "\t")
+helix <- read.table("data/helix.txt", sep = "\t")
+twinpeaks <- read.table("data/twinpeaks.txt", sep = "\t")
+openBox <- read.table("data/openBox.txt", sep = "\t")
+
+#### Optimize the parameters ####
+## ATTENTION: Take a lot of time. Run only once.
+
+source("optimize_parameters.R")
+
+## using 4 cores :
+cl <- makeCluster(4)
+clusterExport(cl)
+
+## Run on 2 seperated computers
+test1 = c(10, 15, 25, 50) #margot
+test2 = c(5, 20, 30, 40) #thu
+
+## Swiss Roll
+d_swiss <- dist(swissRoll)
+# optimize_k_isomap(d_swiss, test1) 
+optimize_k_isomap(d_swiss, test2)
+
+stopCluster(cl)
 
 
--- optimize_parameters.R
+++ optimize_parameters.R
@@ -1,20 +1,8 @@
-optimize_k_isomap <- function(ndim, test_k){
+optimize_k_isomap <- function(distances, ndim, test_k){
     foreach(i= test_k) %dopar% {
       x_iso <- isomap(distances, ndim=2, k=i)
-      png(file = file.path("plots", "plot_test_isomap", paste("k", i, ".png", sep="")),  bg = "transparent")
-      barplot(x_iso$eig[1:10], main = paste("Inertie = ",sum(x_iso$eig[1:2])/sum(x_iso$eig), sep=""))
+      png(file = file.path("plots", "test_isomap", paste("k", i, ".png", sep="")),  bg = "transparent")
+      barplot(x_iso$eig[1:10], main = paste("Inertie = ", sum(x_iso$eig[1:2])/sum(x_iso$eig), sep=""))
       dev.off()
     }
 }
-
-# using 4 cores :
-require(doParallel)
-require(foreach)
-cl <- makeCluster(4)
-clusterExport(cl)
-
-test1 = c(10, 15, 25, 50) #margot
-test2 = c(5, 20, 30, 40) #thu
-
-optimize_k_isomap(2, test2)
-stopCluster(cl)
