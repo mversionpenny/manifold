c415ae883007e6cf9d4f24d8617e75972ca4effc
Hoai Thu Nguyen <hoaithu.nguyen.26.4.92@gmail.com>
2016-12-10 14:22:30 +0100

    finish test k for isomap except for broken swiss roll

--- optimize_parameters.R
+++ optimize_parameters.R
@@ -5,7 +5,7 @@ optimize_k_isomap <- function(distances, ndim = 2, test_k, dataname){
       require(vegan)
       x_iso <- isomap(distances, ndim=2, k=i, fragmentedOK = T)
       png(file = file.path("plots", paste("test_isomap_", dataname, sep=''), paste("k", i, ".png", sep="")),  bg = "transparent")
-      barplot(x_iso$eig[1:10], main = paste("Inertie = ", sum(x_iso$eig[1:2])/sum(x_iso$eig), sep=""))
+      barplot(x_iso$eig[1:10], main = paste("Inertie = ", sum(abs(x_iso$eig[1:2]))/sum(abs(x_iso$eig)), sep=""))
       dev.off()
     }
 }
