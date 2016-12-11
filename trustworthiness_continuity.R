12fb6bf5afb867209ef6518396871768bb7d1c3e
mversionpenny <margot.selosse@gmail.com>
2016-12-11 13:13:26 +0100

    continuity DONE

--- trustworthiness_continuity.R
+++ trustworthiness_continuity.R
@@ -8,20 +8,14 @@ library(RANN)
 trustworthiness <- function(k, HD_data, LD_data){
   
   n <- nrow(HD_data)
-  totalSum <-0
   print("getting hd neighborhoods")
   hd.ngh <- nn2(HD_data, k=n)
   print("getting ld neighborhoods")
   ld.ngh <- nn2(LD_data, k=k)
-  distances <- dist(LD_data)
   
   sum <- 0
   for (i in 1:n){
-    
-    # sum <- sum + Reduce("+", sapply(1:k, FUN = function(m){
-    #           if(is.na(match(ld.ngh$nn.idx[i,m],hd.ngh$nn.idx[i,]))) (m-k)
-    #           else 0
-    #         }))
+
     sum <- sum + Reduce("+", sapply(1:k, FUN = function(m){
       if(is.na(match(ld.ngh$nn.idx[i,m],hd.ngh$nn.idx[i,1:k]))){
         rank <- which(hd.ngh$nn.idx[i,] == ld.ngh$nn.idx[i,m])
@@ -35,6 +29,25 @@ trustworthiness <- function(k, HD_data, LD_data){
   return(result)
 }
 
-continuity <- function(){
+continuity <- function(k, HD_data, LD_data){
+  n <- nrow(HD_data)
+  print("getting hd neighborhoods")
+  hd.ngh <- nn2(HD_data, k=k)
+  print("getting ld neighborhoods")
+  ld.ngh <- nn2(LD_data, k=n)
   
+  sum <- 0
+  for (i in 1:n){
+    
+    sum <- sum + Reduce("+", sapply(1:k, FUN = function(m){
+      if(is.na(match(hd.ngh$nn.idx[i,m],ld.ngh$nn.idx[i,1:k]))){
+        rank <- which(ld.ngh$nn.idx[i,] == hd.ngh$nn.idx[i,m])
+        rank-k
+      } 
+      else 0
+    }))
+    
+  }
+  result <- 1 - 2/(n*k*(2*n - 3*k -1)) * sum
+  return(result) 
 }
