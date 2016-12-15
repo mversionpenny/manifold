ff5660b400928f8916f03014b235e07d74a18384
Hoai Thu Nguyen <hoaithu.nguyen.26.4.92@gmail.com>
2016-12-15 13:10:35 +0100

    plot real data

--- plot_results.R
+++ plot_results.R
@@ -45,12 +45,12 @@ for (i in 1:length(data_name)){
 
 #### Real data ####
 classes <- c("apple", "car", "cow", "cup", "dog", "horse", "pear", "tomato")
-factor_class <- as.factor(rep(classes, each = n/8))
 n = nrow(real_seg)
+factor_class <- as.factor(rep(classes, each = n/8))
 col.classes <- brewer.pal(8,"Dark2")
 cols.classes <- rep(col.classes, each=n/8)
 data_name <- c("real_seg", "real_grey", "real_color")
-best_iso <- c()
+best_iso <- c(9, 5, 5)
 best_lle <- c(18,24,15)
 
 ## Sammon
@@ -64,27 +64,23 @@ for (name in data_name){
 }
 
 ## Isomap
-for (name in data_name){
-  load(file.path("data", paste("test_isomap_", data_name[i], sep=""), paste("k", best_iso[i], ".RData", sep=""))) #load x_iso
-  if (i == 1) points <- x_iso$points[,1]
-  else points <- x_iso$points
+for (i in 1:length(data_name)){
+  name = data_name[i]
+  load(file.path("data", paste("test_isomap_", name , sep=""), paste("k", best_iso[i], ".RData", sep=""))) #load x_iso
+  points <- as.data.frame(x_iso$points)
   p <- ggplot(points, aes(x=points[,1], y=points[,2])) + 
     geom_point(aes(colour = factor_class)) + 
     xlab("") + ylab("") + scale_color_manual(values=brewer.pal(8, "Dark2"))
-  ggsave(file.path("plots", "results_sammon", paste(name, ".png", sep="")), plot = p, width = 7, height = 6)
+  ggsave(file.path("plots", "results_isomap", paste(name, ".png", sep="")), plot = p, width = 7, height = 6)
 }
 
 ## LLE
-for (name in data_name){
-  load(file.path("data", "sammon", paste(name,".RData", sep=""))) # load sammon_real_...(seg, grey or color)
-  points <- as.data.frame(get(paste("sammon", name, sep="_"))$points)
+for (i in 1:length(data_name)){
+  name = data_name[i]
+  load(file.path("data", "lle", paste(name, "_k", best_lle[i],".RData", sep=""))) # load x_lle
+  points <- as.data.frame(x_lle$Y)
   p <- ggplot(points, aes(x=points[,1], y=points[,2])) + 
     geom_point(aes(colour = factor_class)) + 
     xlab("") + ylab("") + scale_color_manual(values=brewer.pal(8, "Dark2"))
-  ggsave(file.path("plots", "results_sammon", paste(name, ".png", sep="")), plot = p, width = 7, height = 6)
+  ggsave(file.path("plots", "results_lle", paste(name, ".png", sep="")), plot = p, width = 7, height = 6)
 }
-
-load(file.path("data", "lle", "real_seg_k18.RData")) #load sammon_real_seg
-# png(filename = file.path("plots", "results_sammon", paste(name, ".png", sep="")))
-plot(x_lle$Y, col = cols, pch = 16, xlab = "", ylab="")
-legend(3, 1, legend = classes, pch =16, col = col, xpd=TRUE)
